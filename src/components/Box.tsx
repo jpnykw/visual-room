fab7f22523089a9eb9aa7719585487ae7c0ce845
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-05-24 08:22:20 +0900

    :construction: setup for post-processing demo in part

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,20 +1,26 @@
-import React, { useRef } from 'react'
 import { Mesh } from 'three'
-import { useFrame } from 'react-three-fiber'
+import React, { useRef } from 'react'
+import { useFrame, extend, useThree } from 'react-three-fiber'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
+
+extend({ EffectComposer, RenderPass, ShaderPass })
 
 const Box: React.FC = () => {
   const ref = useRef({} as Mesh)
   useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01))
 
+  // post-processing 用の設定
+  const { gl, scene, camera, size } = useThree()
+
   return (
-   <mesh
-      ref={ref}
-      position={[0, 0, 0]}
-      scale={[1.3, 1.3, 1.3]}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color='#b82f1a' />
-    </mesh>
+    <>
+      <mesh ref={ref}>
+        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+        <meshStandardMaterial attach="material" color='#b82f1a' />
+      </mesh>
+    </>
   )
 }
 
