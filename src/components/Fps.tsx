8de5022a8ab4c6bc2d8fe7c81bb81b73a3f75b7d
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-05-24 07:22:19 +0900

    :sparkles: update styles for FPS

--- src/components/Fps.tsx
+++ src/components/Fps.tsx
@@ -1,6 +1,7 @@
 // フレームレート表示
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
 interface FpsProps {
   fps: number
@@ -8,9 +9,16 @@ interface FpsProps {
 
 const Fps: React.FC<FpsProps> = (props) => {
   return (
-    <h4>{props.fps} FPS</h4>
+    <Label>
+      {props.fps} FPS
+    </Label>
   )
 }
+
+const Label = styled.div`
+  font-size: 27px;
+  margin-left: 10px;
+`
 
 export default Fps
 
