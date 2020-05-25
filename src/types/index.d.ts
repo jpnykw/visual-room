60737e911cc9c67a4094d181d8d8d7d706e40eed
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-05-26 04:36:53 +0900

    :recycle: replace to spaces and remove semicolons

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,14 +1,14 @@
-import { ReactThreeFiber } from 'react-three-fiber';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { ReactThreeFiber } from 'react-three-fiber'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
 
 declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            effectComposer: ReactThreeFiber.Node<EffectComposer,typeof EffectComposer>;
-            renderPass: ReactThreeFiber.Node<RenderPass, typeof RenderPass>;
-            shaderPass: ReactThreeFiber.Node<ShaderPass, typeof ShaderPass>;
-        }
+  namespace JSX {
+    interface IntrinsicElements {
+      effectComposer: ReactThreeFiber.Node<EffectComposer,typeof EffectComposer>
+      renderPass: ReactThreeFiber.Node<RenderPass, typeof RenderPass>
+      shaderPass: ReactThreeFiber.Node<ShaderPass, typeof ShaderPass>
     }
+  }
 }
