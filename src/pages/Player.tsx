f9b4f5a5e6e0278fa2fc68d8216d232d7059c81e
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-05-26 22:41:54 +0900

    :sparkles: add the wire to back to top

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,13 +1,30 @@
 // プレイヤーの実装
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUndoAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Player: React.FC = () => {
   return (
     <>
+      <Back>
+        <Link to="/">
+          <FontAwesomeIcon icon={faUndoAlt} />
+        </Link>
+      </Back>
     </>
   )
 }
+
+const Back = styled.div`
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 24px;
+`
 
 export default Player
 
